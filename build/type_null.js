05489bd3607076316859b1172b0fa22fe554dea2
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-02-24 11:32:13 -0300

    create null and undefined type in ts

---
+++ build/type_null.js
@@ -0,0 +1,8 @@
+"use strict";
+let nome;
+nome = "João";
+console.log(nome);
+nome = null;
+console.log(nome);
+let nomeUser;
+console.log(nomeUser);
---
+++ src/type_null.ts
@@ -0,0 +1,13 @@
+// Type Null
+
+let nome: string | null;
+
+nome = "João";
+
+console.log(nome);
+
+nome = null;
+console.log(nome);
+
+let nomeUser;
+console.log(nomeUser);
