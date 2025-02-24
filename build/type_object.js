fb50cca096f2ccad3d7114597f445f5da1275f7a
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-02-24 11:22:53 -0300

    create object type in ts

---
+++ build/type_object.js
@@ -0,0 +1,9 @@
+"use strict";
+let novoUsuario = {
+    nome: "João",
+    email: "joao@test.com",
+    idade: 30,
+    telefone: "11999999999",
+    ativo: true,
+};
+console.log(novoUsuario);
---
+++ src/type_object.ts
@@ -0,0 +1,11 @@
+// Type object
+
+let novoUsuario: object = {
+  nome: "João",
+  email: "joao@test.com",
+  idade: 30,
+  telefone: "11999999999",
+  ativo: true,
+};
+
+console.log(novoUsuario);
