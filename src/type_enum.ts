cab21cf067ac0dcc018e7d8e0b65a89ec1e17ccc
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-02-24 11:27:34 -0300

    create enum type in ts

---
+++ src/type_enum.ts
@@ -0,0 +1,19 @@
+// Type Enum
+
+enum DesignColors {
+  white = "#FFFFFF",
+  black = "#000000",
+}
+
+console.log(DesignColors.white);
+console.log(DesignColors.black);
+
+enum StatusPermission {
+  ADMIN,
+  USER,
+  SUPPORT,
+}
+
+console.log(StatusPermission.ADMIN);
+console.log(StatusPermission.USER);
+console.log(StatusPermission.SUPPORT);
