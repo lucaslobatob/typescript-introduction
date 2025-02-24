c02632cbffa90644b7b0f5a07e44d675cb64a518
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-02-24 12:35:30 -0300

    working with functions in ts

--- build/functions/funcoes.js
+++ build/functions/funcoes.js
@@ -6,3 +6,13 @@ function soma(valor1, valor2) {
     return soma;
 }
 console.log(soma(n1, parseInt(n2)));
+function somando(valor1, valor2) {
+    let soma = valor1 + valor2;
+    if (soma > 20) {
+        return "Valor maior que 20";
+    }
+    else {
+        return "Valor menor que 20";
+    }
+}
+console.log(somando(n1, parseInt(n2)));
--- src/functions/funcoes.ts
+++ src/functions/funcoes.ts
@@ -19,3 +19,15 @@ function soma(valor1: number, valor2: number): number {
 }
 
 console.log(soma(n1, parseInt(n2)));
+
+function somando(valor1: number, valor2: number): string {
+  let soma = valor1 + valor2;
+
+  if (soma > 20) {
+    return "Valor maior que 20";
+  } else {
+    return "Valor menor que 20";
+  }
+}
+
+console.log(somando(n1, parseInt(n2)));
